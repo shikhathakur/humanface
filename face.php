f13bc607a5a7d28a84115d67447b237df8bb0625
unknown <sthakur@IM-RT-IT-161.innomindshyd.com>
2015-12-15 17:46:18 +0530

    human face using html and css

---
+++ face.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="face.css">
+</head>
+
+<body>
+  <div class="face">
+  
+    <div class="hover" >
+      
+            <div id="leye">
+            <p id="eyeball">
+            </p></div>
+           <div class="appear" >
+          <div class="arrow-down" margin-left:1px> 
+          </div> 
+             this is left eye
+      </div> 
+    </div>
+  
+    
+           <div class="hover" style="float:right">
+            <div id="reye" >
+            <p id="eyeball">
+             </p>
+          </div>
+          <div class="appear" >
+          <div class="arrow-down" margin-left:1px> 
+          </div> 
+             this is right eye
+      </div>
+      </div>
+         
+  
+          <div class="hover" ><div id="lnose" >
+          </div>
+           <div class="appear" >
+          <div class="arrow-down" margin-left:1px> 
+          </div> 
+             this is left nose
+      </div>
+      </div>
+          
+          
+         <div class="hover" style="float:right" ><div id="rnose">
+        </div>
+          <div class="appear" >
+          <div class="arrow-down" margin-left:1px> 
+          </div> 
+             this is right nose
+      </div>
+      </div>
+ 
+    
+    
+         <div class="hover"  > <div id="smile">
+          <svg width="100" height="100">
+          <circle cx="50" cy="-10" r="40" stroke="black" stroke-width="4" fill="lightgrey" />
+          </div>
+           <div class="appear" >
+          <div class="arrow-down" margin-left:1px> 
+          </div> 
+             this is smile
+      </div>
+      </div>
+    
+  </div>
+
+</body>
+
+</html>
